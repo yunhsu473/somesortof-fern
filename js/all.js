412108a3d64706bf7ca7de612a86ed5d30e10250
yunhsu473 <yunhsu473@gmail.com>
2019-08-14 14:16:34 +0800

    first upload

---
+++ js/all.js
@@ -0,0 +1,9 @@
+// --------home page-------
+$('document').ready(function () {
+    console.log('load');
+    $('.jumbotron').fadeIn(1000);
+    $('.container').fadeIn(2500);
+    $('.list').fadeIn(2500);
+});
+
+// -------intro----
