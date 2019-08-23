3a0d8af87af40ab84b9ca58940b11a4dbf7bd745
yunhsu473 <yunhsu473@gmail.com>
2019-08-23 16:39:35 +0800

    update intro

--- js/all.js
+++ js/all.js
@@ -7,3 +7,10 @@ $('document').ready(function () {
 });
 
 // -------intro----
+
+// --------news-------
+$('.accordion').on('click', '.accordion-control', function (e) {
+    e.preventDefault();
+    /*被點到的東西他弟，沒有animated的全部（進行中不會受影響），展開或隱藏*/
+    $(this).next('.accordion-panel').not(':animated').slideToggle();
+})
